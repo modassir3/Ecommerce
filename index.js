f76d54f142728a78d5b265fa023e90c9e9fac34c
modassir3 <modassirraza34@gmail.com>
2025-03-26 17:51:33 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// Select the element with the class "book-a-call"
+document.querySelector('.book-a-call').addEventListener('click', function () {
+    // Redirect to a different page
+    window.location.href = 'https://example.com'; // Replace with your desired URL
+});
